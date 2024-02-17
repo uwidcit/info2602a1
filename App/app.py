92587b3c576007661b1cfe1fab4a8c5200c23b64
Nicholas Mendez <snickdx@gmail.com>
2024-02-17 03:02:33 +0000

    updated deps and template

--- App/app.py
+++ App/app.py
@@ -1,33 +1,41 @@
-import os
-from datetime import timedelta
 from flask import Flask, jsonify, request
+from functools import wraps
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token, jwt_required, JWTManager, get_jwt_identity
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    get_jwt_identity,
+    jwt_required,
+    set_access_cookies,
+    unset_jwt_cookies,
+)
+
 from .models import db, User, UserPokemon, Pokemon
 
+# Configure Flask App
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'MySecretKey'
+app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
+app.config['JWT_REFRESH_COOKIE_NAME'] = 'refresh_token'
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config["JWT_COOKIE_SECURE"] = True
+app.config["JWT_SECRET_KEY"] = "super-secret"
+app.config["JWT_COOKIE_CSRF_PROTECT"] = False
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
-    app.config['DEBUG'] = True
-    app.config['SECRET_KEY'] = 'MySecretKey'
-    app.config["JWT_SECRET_KEY"] = "super-secret"
-    app.config['PREFERRED_URL_SCHEME'] = 'https'
-    app.config["JWT_TOKEN_LOCATION"] = ["headers"]
-    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=12)
-    CORS(app)
-    db.init_app(app)
-    app.app_context().push()
-    return app
 
-app = create_app()
-jwt = JWTManager(app)  #setup flask jwt-e to work with app
+# Initialize App 
+db.init_app(app)
+app.app_context().push()
+CORS(app)
+jwt = JWTManager(app)
 
+# ********** Routes **************
 @app.route('/')
 def index():
   return '<h1>Poke API</h1>'
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=81)
+  app.run(host='0.0.0.0', debug=True)
